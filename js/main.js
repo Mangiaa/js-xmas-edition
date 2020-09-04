a4c6966a3712e8de949602b0e2317d02846d4627
Mangiaa <lucianomangiaterra@gmail.com>
2020-09-03 21:17:44 -0300

    Cambio del formato
    Le cambie el formato a todos los archivos por que se veia mas bonito usando prettier

--- js/main.js
+++ js/main.js
@@ -16,108 +16,92 @@ let resultadoFinal = obtenerRadio()
 
 alert(resultadoFinal)*/
 
-
-
 function validarNombre(nombre) {
-    if (nombre.length === 0) {
-        return "Este campo debe tener al menos 1 caracter"
-    }
-    if (nombre.length >= 50) {
-        return "Este campo debe tener menos de 50 caracteres"
-    }
-    if (!/^[a-z]+$/i.test(nombre)) {
-        return 'El campo nombre solo acepta letras'
-    }
-    return ""
+  if (nombre.length === 0) {
+    return "Este campo debe tener al menos 1 caracter";
+  }
+  if (nombre.length >= 50) {
+    return "Este campo debe tener menos de 50 caracteres";
+  }
+  if (!/^[a-z]+$/i.test(nombre)) {
+    return "El campo nombre solo acepta letras";
+  }
+  return "";
 }
-
-
-
 
 function validarDescripcionRegalo(descripcionRegalo) {
-    if (descripcionRegalo.length >= 100) {
-        return "El texto que ingresaste es muy largo"
-    } else if (descripcionRegalo.length === 0) {
-        return "Por favor ingresa un texto valido"
-    } else if (!/^[a-z0-9,\._ ]+$/i.test(descripcionRegalo)) {
-        return "Por favor ingrese un caracter valido"
-    }
-    return ''
+  if (descripcionRegalo.length >= 100) {
+    return "El texto que ingresaste es muy largo";
+  } else if (descripcionRegalo.length === 0) {
+    return "Por favor ingresa un texto valido";
+  } else if (!/^[a-z0-9,\._ ]+$/i.test(descripcionRegalo)) {
+    return "Por favor ingrese un caracter valido";
+  }
+  return "";
 }
-
-
 
 function validarCiudad(ciudad) {
-    if (ciudad === "") {
-        return "Por Favor ingrese una provincia"
-    }
-    return ""
+  if (ciudad === "") {
+    return "Por Favor ingrese una provincia";
+  }
+  return "";
 }
 
-const $form = document.querySelector('#carta-a-santa')
+const $form = document.querySelector("#carta-a-santa");
 
 function validarFormulario(event) {
+  event.preventDefault();
 
-    event.preventDefault()
+  const nombre = $form.nombre.value;
+  const ciudad = $form.ciudad.value;
+  const descripcionRegalo = $form["descripcion-regalo"].value;
 
-    const nombre = $form.nombre.value
-    const ciudad = $form.ciudad.value
-    const descripcionRegalo = $form['descripcion-regalo'].value
+  const errorNombre = validarNombre(nombre);
+  const errorCiudad = validarCiudad(ciudad);
+  const errorDescripcionRegalo = validarDescripcionRegalo(descripcionRegalo);
 
+  const objetoErrores = {
+    nombre: errorNombre,
+    ciudad: errorCiudad,
+    "descripcion-regalo": errorDescripcionRegalo,
+  };
 
-    const errorNombre = validarNombre(nombre)
-    const errorCiudad = validarCiudad(ciudad)
-    const errorDescripcionRegalo = validarDescripcionRegalo(descripcionRegalo)
+  const esExito = manejarErrores(objetoErrores) === 0; //! COMPARO, SI TIENE ERRORES != EXITO, SI ES === 0 ESTONCES ES EXITOSO Y EN 5 SEGUNDOS LO MANDO A WISHLIST
+  if (esExito) {
+    document.querySelector("#carta-a-santa").remove();
+    const $divExito = document.querySelector("#exito");
+    $divExito.className = "";
 
-
-    const objetoErrores = {
-        nombre: errorNombre,
-        ciudad: errorCiudad,
-        'descripcion-regalo': errorDescripcionRegalo
-    }
-
-    const esExito = manejarErrores(objetoErrores) === 0;        //! COMPARO, SI TIENE ERRORES != EXITO, SI ES === 0 ESTONCES ES EXITOSO Y EN 5 SEGUNDOS LO MANDO A WISHLIST
-    if(esExito){
-        document.querySelector('#carta-a-santa').remove()
-        const $divExito = document.querySelector('#exito')
-        $divExito.className = ""
-        
-        setTimeout(() => {
-            window.location.assign("wishlist.html")
-        }, 5000);
-    }
+    setTimeout(() => {
+      window.location.assign("wishlist.html");
+    }, 5000);
+  }
 }
-
-
 
 function manejarErrores(errores) {
-    let contadorErrores = 0
-    const $errores = document.querySelector('#errores')
-    const keys = Object.keys(errores)
-    
-    while($errores.hasChildNodes()){
-        $errores.removeChild($errores.firstChild);
-    } 
-    keys.forEach(function (key) {
-        const error = errores[key]
+  let contadorErrores = 0;
+  const $errores = document.querySelector("#errores");
+  const keys = Object.keys(errores);
 
-        if (error) {
-            contadorErrores++
-            $form[key].className = 'error'
-            let liErrores = document.createElement('li')
-            liErrores.textContent = error
-            $errores.appendChild(liErrores)
-            
+  while ($errores.hasChildNodes()) {
+    $errores.removeChild($errores.firstChild);
+  }
+  keys.forEach(function (key) {
+    const error = errores[key];
 
-        } else {
-           console.log('Exito!')
-            $form[key].className = ''
-            
-        }
-        
-    })
+    if (error) {
+      contadorErrores++;
+      $form[key].className = "error";
+      let liErrores = document.createElement("li");
+      liErrores.textContent = error;
+      $errores.appendChild(liErrores);
+    } else {
+      console.log("Exito!");
+      $form[key].className = "";
+    }
+  });
 
-    return contadorErrores
+  return contadorErrores;
 }
 
-$form.onsubmit = validarFormulario
+$form.onsubmit = validarFormulario;
